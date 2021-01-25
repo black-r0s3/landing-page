893bf1ef11cd2b975e52c4ef9f43e9009b22945b
black-r0s3 <ebenezer.nyarko@amalitech.org>
2021-01-25 17:26:55 +0000

    added a landing page

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// console.log('hello eben')
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("first");
+
+var span = document.getElementsByClassName("close")[0];
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var btn = document.getElementById("second");
+
+
+var span = document.getElementsByClassName("close")[0];
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
